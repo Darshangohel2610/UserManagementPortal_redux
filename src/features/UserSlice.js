d703dd7af6b5c02386c83580af66f0264622776f
Darshangohel2610 <darshangohel32@gmail.com>
2025-02-12 11:34:34 +0530

    last commit

--- src/features/UserSlice.js
+++ src/features/UserSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from "axios"
-const API_URL = 'http://localhost:5000/users';
+const API_URL = 'https://my-json-server.typicode.com/Darshangohel2610/JSON_DB/users';
 
 const uploadFile = (file) => {
     return new Promise((resolve) => {
